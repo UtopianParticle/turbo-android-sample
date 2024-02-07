efe6b7ef06aa78ef0e63b1e1c771fc5becaf83e1
Ivo Jesus <ivo.jesus@gmail.com>
2024-02-07 00:28:31 +0000

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import dev.hotwire.turbo.activities.TurboActivity
+import dev.hotwire.turbo.delegates.TurboActivityDelegate
+
+class MainActivity : AppCompatActivity(), TurboActivity {
+    override lateinit var delegate: TurboActivityDelegate
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        delegate = TurboActivityDelegate(this, R.id.main_nav_host)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainSessionNavHostFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dev.hotwire.turbo.config.TurboPathConfiguration
+import dev.hotwire.turbo.session.TurboSessionNavHostFragment
+import kotlin.reflect.KClass
+
+class MainSessionNavHostFragment : TurboSessionNavHostFragment() {
+    override val sessionName = "main"
+    override val startLocation = "https://dd7b-2001-8a0-7a9f-800-6115-a4b4-eebc-addf.ngrok-free.app"
+
+    override val registeredActivities: List<KClass<out AppCompatActivity>>
+        get() = listOf(
+            // Leave empty unless you have more
+            // than one TurboActivity in your app
+        )
+
+    override val registeredFragments: List<KClass<out Fragment>>
+        get() = listOf(
+            WebFragment::class
+            // And any other TurboFragments in your app
+        )
+
+    override val pathConfigurationLocation: TurboPathConfiguration.Location
+        get() = TurboPathConfiguration.Location(
+            assetFilePath = "json/configuration.json",
+            remoteFileUrl = "https://turbo.hotwired.dev/demo/configurations/android-v1.json"
+        )
+}
